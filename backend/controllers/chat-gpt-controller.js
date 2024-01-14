9fc7b63d1be8f0cfe83edfa46974377d9f0873c2
SomnathKar000 <somnathkar201901@gmail.com>
2024-01-14 21:42:08 +0530

    added few changes

--- backend/controllers/chat-gpt-controller.js
+++ backend/controllers/chat-gpt-controller.js
@@ -1,43 +1,37 @@
-// const { Configuration, OpenAIApi } = require("openai");
 const OpenAI = require("openai");
 
-// const config = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openAi = new OpenAIApi(config);
-
 const getAnswers = async (question) => {
-  const openai = new OpenAI({ key: process.env.OPENAI_API_KEY });
-  // const responce = await openAi.createChatCompletion({
-  //   model: "gpt-3.5-turbo",
-  //   messages: [{ role: "user", content: question }],
-  //   temperature: 0.9,
-  //   max_tokens: 150,
-  //   top_p: 1,
-  //   frequency_penalty: 0.0,
-  //   presence_penalty: 0.6,
-  //   stop: [" Human:", " AI:"],
-  // });
-  // console.log();
-  // const messageArray = responce.data.choices[0].message.content
-  //   .split("\n")
-  //   .filter((line) => line !== "" && !line.startsWith("```"));
-  // return messageArray;
-  const prompt = question;
+  try {
+    const openai = new OpenAI({ key: process.env.OPENAI_API_KEY });
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: prompt },
-    ],
-    model: "gpt-3.5-turbo",
-  });
+    const prompt = question;
 
-  console.log(completion.choices[0]);
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: prompt },
+      ],
+      model: "gpt-3.5-turbo",
+    });
 
-  const generatedText = completion.choices[0];
-  return generatedText;
+    console.log(completion.choices[0]);
+
+    const messageArray = completion.choices[0].message.content
+      .split("\n")
+      .filter((line) => line !== "" && !line.startsWith("```"));
+    return {
+      success: true,
+      messageArray,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      messageArray: [
+        "You exceeded your current quota, please check your plan and billing details.",
+        " Learn more at https://platform.openai.com/account/billing/plan",
+      ],
+    };
+  }
 };
 
 module.exports = getAnswers;
